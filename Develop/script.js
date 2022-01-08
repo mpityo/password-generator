dc804d3fdf45d5719ff8d75c3ab8f3ce138747a5
Matt Pityo <mpityo@gmail.com>
2022-01-08 12:30:21 -0500

    finish capturing input and processing

--- Develop/script.js
+++ Develop/script.js
@@ -7,7 +7,34 @@ var confirmSelection = document.querySelector('#confirm');
 var popUp = document.getElementById("pop-up-selection");
 var close = document.getElementsByClassName("close")[0];
 
-// Get input from user of what they want in their password
+var getPasswordLimits = function() {
+	// set up variables and get info for user selections
+	var oneLimitSelected = false;
+	var passwordLength = document.getElementById("passwordLength").value;
+	var checkboxes = document.querySelectorAll('input[name="rules"]:checked');
+	var rules = [];
+	checkboxes.forEach((checkbox) => {
+		rules.push(checkbox.id);
+	});
+	
+	// pull selections and run through passwordRules to enable only what the user selected
+	for (var i = 0; i < rules.length; i++) {
+		var index = parseInt(rules[i]);
+		passwordRules[index].include = true;
+	}
+	
+	if (rules.length < 1) {
+		window.alert("No rules were selected, try again.");
+	} else {
+		if (passwordLength > 7 && passwordLength < 129) {
+			return parseInt(passwordLength);
+		} else {
+			window.alert("Password must be between 8 and 128 characters.");
+		}
+	}
+	 return 0;
+}
+/* Get input from user of what they want in their password
 var getPasswordLimits = function() {
 	var oneLimitSelected = false;
 	// ask user for CHARACTERS they WANT
@@ -40,7 +67,7 @@ var getPasswordLimits = function() {
 	
 	// only returns length if it passes the do-while loop
 	return length;
-}
+}*/
 
 // Generate the password using how long and what characters the user selected
 var generatePassword = function(passwordLength) {
@@ -58,6 +85,7 @@ var generatePassword = function(passwordLength) {
 	for (var i = 0; i < passwordLength; i++) {
 		// get a random number between 0 and the length of characters the user wanted
 		var randomNumber = Math.floor(Math.random() * arrayOfCharacters.length);
+		// parse characters for the random number and add that to the password
 		password += arrayOfCharacters[randomNumber];
 	}
 	return password;
@@ -76,22 +104,22 @@ function writePassword() {
 var passwordRules = [
 	{
 		item: "uppercase letters",
-		include: true,
+		include: false,
 		characters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 	},
 	{
 		item: "lowercase letters",
-		include: true,
+		include: false,
 		characters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	},
 	{
 		item: "numbers",
-		include: true,
+		include: false,
 		characters: ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 	},
 	{
 		item: "special characters",
-		include: true,
+		include: false,
 		characters: ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '/']
 	}
 ];
@@ -113,7 +141,7 @@ window.onclick = function(event) {
 	}
 }
 
+// Send to pop up
+generateBtn.addEventListener("click", generateBtn.onClick);
 // Button in pop up after user selects checkboxes
 confirmSelection.addEventListener("click", writePassword);
-// Send to pop up
-generateBtn.addEventListener("click", generateBtn.onClick)
